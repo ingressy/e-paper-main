78d5bea5432a27176bbcdee8f3b1b710546376be
PJoK24 <herculesdj@web.de>
2024-10-30 14:57:19 +0100

    .

--- server-epaper.py
+++ server-epaper.py
@@ -1,1 +1,4 @@
-from I
+from IMAGEGEN.imagegen2 import gen_image
+
+gen_image("2.312", "1", "2", "LIB", "ENG", "BGT241", "1", "3", "4", "WIB", "DEU", "BGT241", "0", "5", "6", "STT", "MAT",
+          "BGT241", "0")
